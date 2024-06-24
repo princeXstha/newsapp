138a8634a27bb9aad5c5c544356a5d2f82bb21ae
Prince Shrestha <princestha2004@gmail.com>
2024-06-24 18:52:46 +0545

    converting into function based

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect} from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import propTypes from 'prop-types'
@@ -6,64 +6,47 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 
 
-export class News extends Component {
-    static defaultProps={
-        country: 'in',
-        pagesize: 12,
-        category: 'general'
-    }
-    static propTypes = {
-        country: propTypes.string,
-        pagesize: propTypes.number,
-        category: propTypes.string
-    }
-    constructor(){
-        super();
-        this.state={
-            articles: [],
-            loading: true,
-            page: 1,
-            totalResults: 0
-        }
-    }
-    async updateNews(){
-        const url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=62c2c30bc94745e69543902d012e19d4&page={this.state.page}&pageSize=${this.props.pagesize}`;
-        this.setState({loading: true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({articles: parsedData.articles,
-             loading: false,
-             totalResults : parsedData.totalResults})
+const News =(props)=>{
+    const [articles, setArticles]= useState([])
+    const [loading, setLoading]= useState(true)
+    const [page, setPage]= useState(1)
+    const [totalResults, settotalResult]= useState(0);
 
-    }
-    async componentDidMount(){
-        this.updateNews(1);
-    }
-    fetchMoreData = async () => {
-          this.setState({
-                page: this.state.page +1})
-        const url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=62c2c30bc94745e69543902d012e19d4&page={this.state.page}&pageSize=${this.props.pagesize}`;
+    const updateNews = async ()=>{
+        const url =`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=62c2c30bc94745e69543902d012e19d4&page=${page}&pageSize=${props.pagesize}`;
+        setLoading(true)
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({articles: this.state.articles.concat(parsedData.articles),
-             totalResults : parsedData.totalResults})
+        setArticles(parsedData.articles);
+        settotalResult(parsedData.totalResults)
+        setLoading(false);
+    }
+    useEffect(()=>{
+            updateNews();
+    })
+    const fetchMoreData = async () => {
+        const url =`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=62c2c30bc94745e69543902d012e19d4&page=${page}&pageSize=${props.pagesize}`;
+        setPage(page+1)
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        setArticles(articles.concat(parsedData.articles))
+        settotalResult(parsedData.totalResults)
       };
-  render() {
-    const cat = this.props.category;
+    const cat = props.category;
     return (
         <>
       <div className='container my-4'>
         <h1 className='text-center'>NEWS MONKEY - HEADLINES</h1>
             <h2 className='text-center'>{cat.toUpperCase()}</h2>
-        {this.state.loading && <Spinner/>}
+        {loading && <Spinner/>}
         <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length != this.state.totalResults}
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length !== totalResults}
           loader={<Spinner/>}
         >
         <div className="row">
-        {this.state.articles.map((element)=>{
+        {articles.map((element)=>{
             
              return <div className="col md-4 mt-2" key={element.url}> {/*give a unique key to the thing you are returning*/}
              <NewsItem source={element.source.name} author={element.author} date={element.publishedAt} title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,85):""} imageUrl={!element.urlToImage?"https://static.toiimg.com/thumb/msid-111130453,width-1070,height-580,imgsize-194682,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg":element.urlToImage} newsUrl={element.url}/>
@@ -74,7 +57,16 @@ export class News extends Component {
       </div>
       </>
     )
-  }
+}
+News.defaultProps={
+    country: 'in',
+    pagesize: 12,
+    category: 'general'
+}
+News.propTypes = {
+    country: propTypes.string,
+    pagesize: propTypes.number,
+    category: propTypes.string
 }
 
 export default News
